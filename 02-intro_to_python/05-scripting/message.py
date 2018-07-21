e6faa545c1b9e57c6a04bee715a49fce6e509562
Jan Carbonell <jcllobet@gmail.com>
2018-07-22 00:26:02 +0200

    message.py correction added, much more simple code

--- 02-intro_to_python/05-scripting/message.py
+++ 02-intro_to_python/05-scripting/message.py
@@ -26,3 +26,21 @@ for e in grades_list:
 for e in range(len(names_list)):
 
   print(message.format(names_list[e],assignments_list[e],grades_list[e],max_grade))
+
+# valuable lesson, for can be done with several variables and iterables at the same time:
+
+"""
+Correct solution:
+names = input("Enter names separated by commas: ").title().split(",")
+assignments = input("Enter assignment counts separated by commas: ").split(",")
+grades = input("Enter grades separated by commas: ").split(",")
+
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
+
+for name, assignment, grade in zip(names, assignments, grades):
+    print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
+
+
+"""
